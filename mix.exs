f5c5dc201b413fba7f58f771b9fade295acb3206
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-08-23 23:35:28 +0200

    Prepare project to be publish to Hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Freelixir.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -28,5 +30,19 @@ defmodule Freelixir.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.9.0"}]
+  end
+
+  defp description do
+    """
+    Send SMS to yourself with Free Mobile API.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Jean Bertrand"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/btrd/freelixir"}
+    ]
   end
 end
