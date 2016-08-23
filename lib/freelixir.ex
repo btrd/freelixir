f8b9555d4fa2e0663c7200c2c20ebeb2866aa84e
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-08-23 22:43:34 +0200

    Add function sens_sms that take user, pass, message and send it to Free Mobile API

--- lib/freelixir.ex
+++ lib/freelixir.ex
@@ -1,2 +1,13 @@
 defmodule Freelixir do
+
+  def send_sms(user: user, password: password, message: message) do
+    url = "https://smsapi.free-mobile.fr/sendmsg"
+    cond do
+      String.valid?(user) && String.valid?(password) && String.valid?(message) ->
+        HTTPoison.get("#{url}?user=#{user}&password=#{password}&msg=#{message}") |> check_status
+      true ->
+        {:error, "Freelixir only accept string parans"}
+    end
+  end
+
 end
