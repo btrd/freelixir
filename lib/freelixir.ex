f4d3ac35f5b2e59af7e861409bbbba920933bb19
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-08-23 23:24:10 +0200

    Add check of status return by Free Mobile

--- lib/freelixir.ex
+++ lib/freelixir.ex
@@ -10,4 +10,24 @@ defmodule Freelixir do
     end
   end
 
+  def check_status(params) do
+    cond do
+      {:ok, res} = params ->
+        case res.status_code do
+          200 ->
+            {:ok, "SMS was sent successfully"}
+          400 ->
+            {:error, "Parameters missing: user, password and msg are required"}
+          402 ->
+            {:error, "To many SMS has been sent, wait few minutes"}
+          403 ->
+            {:error, "You didn't activate the service « Notifications par SMS »"}
+          _   ->
+            {:error, "An error occurred with Free Mobile service"}
+        end
+      {_, res} = params ->
+        {:error, res}
+    end
+  end
+
 end
