523940c2dfaeaa0d9c1145c9b90ba6a0054c84e1
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-08-23 22:42:51 +0200

    Create tests on arguments

--- test/freelixir_test.exs
+++ test/freelixir_test.exs
@@ -2,7 +2,12 @@ defmodule FreelixirTest do
   use ExUnit.Case
   doctest Freelixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "send_sms: reject 3 non string arguments" do
+    assert {:error, _} = Freelixir.send_sms(user: 11634714, password: 234, message: "Hello World !")
   end
+
+  test "send_sms: accept 3 string arguments" do
+    assert {:ok, _} = Freelixir.send_sms(user: "11634714", password: "jTKWtfXEFwB3Er", message: "Hello World !")
+  end
+
 end
