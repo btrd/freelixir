a0244e95aee4cae022398933cd4c3ed2f5991f16
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-08-24 13:21:08 +0200

    Add tests for check_status function

--- test/freelixir_test.exs
+++ test/freelixir_test.exs
@@ -10,4 +10,16 @@ defmodule FreelixirTest do
     assert {:error, _} = Freelixir.send_sms(user: "11634714", password: "jTKWtfXEFwB3Er", message: "Hello World !")
   end
 
+  test "check_status: reply error if params not ok" do
+    assert {:error, _} = Freelixir.check_status({:error, "Error"})
+  end
+
+  test "check_status: reply error if params :ok but status unknown" do
+    assert {:error, _} = Freelixir.check_status({:ok, %{status_code: 999}})
+  end
+
+  test "check_status: reply ok if params :ok and status 200" do
+    assert {:ok, _} = Freelixir.check_status({:ok, %{status_code: 200}})
+  end
+
 end
